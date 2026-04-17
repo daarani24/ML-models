b1d8f31df12a63447cb86c02d1f82e5ad49167f6
daarani24 <daarani2007@gmail.com>
2026-04-17 20:55:22 +0530

    edited pkl files

--- Crop_recommendation/main.py
+++ Crop_recommendation/main.py
@@ -1,5 +1,3 @@
-import os
-print(os.listdir())
 from fastapi import FastAPI
 import joblib
 import numpy as np
@@ -11,29 +9,44 @@ le_crop = joblib.load("le_crop.pkl")
 le_soil = joblib.load("le_soil.pkl")
 le_action = joblib.load("le_action.pkl")
 
+
+def predict_action(crop, soil, land, days):
+    crop_val = le_crop.transform([crop])[0]
+    soil_val = le_soil.transform([soil])[0]
+
+    pred = model.predict([[crop_val, soil_val, land, days]])
+
+    return le_action.inverse_transform(pred)[0]
+
+
+def generate_notification(crop_name, soil, land, days, rainfall):
+    ml_action = predict_action(crop_name, soil, land, days)
+
+    if rainfall > 20:
+        final_action = "Skip Irrigation"
+    else:
+        final_action = ml_action
+
+    return f"{crop_name}: {final_action} now"
+
+
 @app.get("/")
 def home():
     return {"message": "ML API Running 🚀"}
 
+
 @app.post("/predict")
 def predict(data: dict):
     try:
-       
-        crop = data["crop"].lower()
-        soil = data["soil"].lower()
-        land = data["land"]
-        days = data["days"]
+        msg = generate_notification(
+            crop_name=data["crop"],
+            soil=data["soil"],
+            land=data["land"],
+            days=data["days"],
+            rainfall=data["rainfall"]
+        )
 
-        crop_enc = le_crop.transform([crop])[0]
-        soil_enc = le_soil.transform([soil])[0]
-
-        input_data = np.array([[crop_enc, soil_enc, land, days]])
-
-        pred = model.predict(input_data)[0]
-
-        action = le_action.inverse_transform([pred])[0]
-
-        return {"prediction": action}
+        return {"notification": msg}
 
     except Exception as e:
         return {"error": str(e)}
